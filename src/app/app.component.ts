eca32cc99b2ad5eaa154cb0f5ff925e9d9b8e1e9
tamara0715@gmail.com <tamara0715@gmail.com>
2019-06-11 21:31:00 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+}
+
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 }
